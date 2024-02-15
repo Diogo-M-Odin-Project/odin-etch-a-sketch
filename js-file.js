878371c0a00abc4c5ec4b6c59c236b8e9f753c6b
Diogo-M-Odin-Project <158120685+Diogo-M-Odin-Project@users.noreply.github.com>
2024-02-15 16:58:26 +0000

    Squares now change color upon mouseover

--- js-file.js
+++ js-file.js
@@ -4,6 +4,15 @@ function createGrid() {
     for (let i = 0; i < 256; i++) {
         let square = document.createElement('div');
         square.classList.toggle('square');
+
+        square.addEventListener('mouseover', function(event) {
+            let r = Math.floor(Math.random() * 256).toString(16);
+            let g = Math.floor(Math.random() * 256).toString(16);
+            let b = Math.floor(Math.random() * 256).toString(16);
+            event.target.style.backgroundColor = '#' + r + g + b;
+            console.log('#' + r + g + b);
+        });
+
         container.appendChild(square);
     }
 }
